6b2874fc91e2dc7610746bfec4e8a06ca7994e77
Pingvu <nhlpool27@gmail.com>
2023-02-17 14:58:12 +0100

    Add project files.

---
+++ Test-Metoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Metoder_test
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två heltal separerade med * tecknet");
+            Console.WriteLine(" ");
+            string a =Console.ReadLine();
+            Console.WriteLine(" ");
+            string[] b=a.Split('*');
+            int c =int.Parse(b[0]);
+            int d =int.Parse(b[1]);
+            Console.WriteLine(Area(c,d));
+        }
+        static int Area(int bredd, int höjd)
+        {
+            
+            return bredd * höjd;
+        }
+    }
+}
